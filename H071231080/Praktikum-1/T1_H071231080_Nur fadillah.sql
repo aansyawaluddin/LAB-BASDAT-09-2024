73d78a09f16a6988dfc0742e64ad7beee1206c0d
Nurfadillah3011 <Nurfadillah3011>
2024-09-12 17:07:38 +0700

    Tugas 1 : Query Dasar

---
+++ H071231080/Praktikum-1/T1_H071231080_Nur fadillah.sql
@@ -0,0 +1,69 @@
+--- Nama : Nur fadillah
+--- NIM : H071231080
+
+
+
+CREATE DATABASE library
+
+USE library
+
+CREATE TABLE authors(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(100) NOT NULL 
+)
+
+CREATE TABLE books(
+id INT PRIMARY KEY AUTO_INCREMENT,
+isbn CHAR(13),
+title VARCHAR(100) NOT NULL ,
+author_id INT,
+FOREIGN KEY (author_id) REFERENCES authors(id)
+)
+
+
+ALTER TABLE authors
+ADD nationality VARCHAR(50)
+
+
+ALTER TABLE books
+MODIFY isbn CHAR(13) UNIQUE
+
+DESCRIBE authors;
+DESCRIBE books;
+
+
+CREATE TABLE members(
+id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(50) NOT NULL ,
+last_name VARCHAR(50)NOT NULL ,
+email VARCHAR(100) UNIQUE NOT NULL,
+phone_number CHAR(10) NOT NULL,
+join_date DATE NOT NULL,
+membership_type VARCHAR(50) NOT NULL 
+)
+
+ALTER TABLE books(
+MODIFY title VARCHAR(150) NOT NULL,
+MODIFY isbn CHAR(13) UNIQUE NOT NULL,
+MODIFY author_id INT NOT NULL,
+ADD published_year YEAR NOT NULL,
+ADD genre VARCHAR(50)NOT NULL,
+coples_avaible INT NOT NULL 
+)
+
+
+CREATE TABLE borrowings(
+id INT PRIMARY KEY AUTO_INCREMENT,
+member_id INT NOT NULL,
+book_id INT NOT NULL,
+borrow_date DATE NOT NULL,
+return_date DATE NOT NULL,
+FOREIGN KEY (member_id) REFERENCES members(id),
+FOREIGN KEY (book_id) REFERENCES books(id)
+)
+
+ALTER TABLE authors
+MODIFY id INT NOT NULL,
+MODIFY nationality VARCHAR(50) NOT NULL 
+
+DROP DATABASE library;
